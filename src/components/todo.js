a8728f92603fbcd9bb1ee124711e208349629491
Martin Vera <m.vera1996@gmail.com>
2022-07-06 17:33:50 -0700

    first commit

---
+++ src/components/todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todo = () => {
+    return (
+        <div>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </div>
+    )
+}
+
+export default Todo
